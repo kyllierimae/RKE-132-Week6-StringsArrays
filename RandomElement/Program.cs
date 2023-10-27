2092689a01c1023c82ebd3d3c773cbd7cd22237e
kuerim <kuerim@taltech.ee>
2023-10-27 18:10:16 +0300

    add week6

---
+++ RandomElement/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "sushi", "Pizza", "Burger", "chicken wings", "Asian takeway" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonaight we are going to eat {snacks[randomIndex]}.");
